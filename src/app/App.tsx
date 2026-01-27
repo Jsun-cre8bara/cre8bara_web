a50fc51ef7877fd0568e5d56710506b90c6eefb4
Jsun-cre8bara <cre8bara@gmail.com>
2026-01-27 20:09:07 +0900

    fix: iframe 酉??섏씠吏?먯꽌 ?명꽣 ?④? 泥섎━

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { Toaster } from './components/ui/sonner';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
@@ -7,20 +7,29 @@ import { FunRunningPage } from '../pages/FunRunningPage';
 import { FunRunningViewPage } from '../pages/FunRunningViewPage';
 import { HomeShoppingViewPage } from '../pages/HomeShoppingViewPage';
 
+function AppContent() {
+  const location = useLocation();
+  const isIframeView = location.pathname === '/funrunning-view' || location.pathname === '/homeshopping-view';
+
+  return (
+    <div className="min-h-screen bg-white">
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/funrunning" element={<FunRunningPage />} />
+        <Route path="/funrunning-view" element={<FunRunningViewPage />} />
+        <Route path="/homeshopping-view" element={<HomeShoppingViewPage />} />
+      </Routes>
+      {!isIframeView && <Footer />}
+      <Toaster />
+    </div>
+  );
+}
+
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-white">
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/funrunning" element={<FunRunningPage />} />
-          <Route path="/funrunning-view" element={<FunRunningViewPage />} />
-          <Route path="/homeshopping-view" element={<HomeShoppingViewPage />} />
-        </Routes>
-        <Footer />
-        <Toaster />
-      </div>
+      <AppContent />
     </BrowserRouter>
   );
 }
