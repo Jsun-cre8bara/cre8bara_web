1b8b0e7363cc5c7f5df722e4ec319128305d3fc3
Jsun-cre8bara <cre8bara@gmail.com>
2026-01-27 19:59:30 +0900

    fix: iframe ?섏씠吏?먯꽌 ?ㅻ뜑媛 蹂댁씠?꾨줉 ?믪씠 怨꾩궛 ?섏젙

--- src/pages/FunRunningViewPage.tsx
+++ src/pages/FunRunningViewPage.tsx
@@ -1,9 +1,10 @@
 export function FunRunningViewPage() {
   return (
-    <div className="pt-16 h-screen flex flex-col">
+    <div className="pt-16 w-full">
       <iframe
         src="https://funrunning-8y2jvuh.gamma.site/"
-        className="w-full flex-1 border-0"
+        className="w-full border-0"
+        style={{ height: 'calc(100vh - 4rem)' }}
         title="펀러닝"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
